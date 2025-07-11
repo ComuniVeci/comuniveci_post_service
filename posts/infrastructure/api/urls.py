60f4c0bd0a91948009c3fb91ddf6084f82234543
JulianAVG64 <julianavg2002@gmail.com>
2025-07-11 15:15:05 -0500

    Correción url nuevo endpoint

--- posts/infrastructure/api/urls.py
+++ posts/infrastructure/api/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('posts/pending/', PostPendingListView.as_view()),
     path("posts/approved/", PostApprovedListView.as_view()),
     path('posts/', PostCreateView.as_view()),
-    path("summary/", PostSummaryView.as_view(), name="post-summary"),
+    path("posts/summary/", PostSummaryView.as_view(), name="post-summary"),
     path('posts/<str:post_id>/approve/', ApprovePostView.as_view()),
     path('posts/<str:post_id>/', PostUpdateView.as_view()),
     path('posts/<str:post_id>/delete/', PostDeleteView.as_view()),
